829c4e5f3e7d3cccf752bced0f8a638a5eea1a5d
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2022-05-18 06:46:19 +0900

    Time: 55 ms (16.18%), Space: 14 MB (12.41%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -3,7 +3,10 @@ class Solution:
         results = [0]*(n+1)
         results[0] = 1
         results[1] = 2
+        p_prev, prev = 1, 2
+        
         for i in range(2,n):
-            results[i] = results[i-1] + results[i-2]
+            results[i] = prev + p_prev
+            p_prev, prev = prev, results[i]
         
         return results[n-1]
