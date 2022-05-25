70fb38ef9cd17ee8823b6911c4f31b5c50b8a857
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2022-05-25 19:25:23 +0900

    Time: 201 ms (55.51%), Space: 15.2 MB (24.21%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        dp = []
+        curr = 0
+        prev = -1
+        while curr < len(nums) - 1:
+            max_reach = self.get_next_reach(nums[prev+1:curr+1])
+            dp.append(max_reach)
+            prev = curr
+            curr += dp[-1]
+            if curr == prev: return 0
+        return len(dp)
+        
+        
+        return results[-1]
+
+    def get_next_reach(self, L):
+        reach = 0
+        for i, num in enumerate(L):
+            reach = max(reach, num - (len(L)-i-1))
+        return reach
