81ccb37f2c76773fe394952505f59d726f1088c7
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2022-07-14 12:49:19 +0900

    Time: 83 ms (42.84%), Space: 14 MB (6.81%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,23 +1,10 @@
 class Solution:
-    def intToRoman(self, num: int) -> str:
-        result = ""
-        d = {"IV":4,"IX":9, "XL":40,"XC":90,"CD":400,"CM":900,
-             "M":1000,"D":500,"C":100,"L":50,"X":10,"V":5,"I":1}
-        
-        thousand = num//1000
-        t = {3:"MMM",2:"MM",1:"M" ,0:""}
-        result += t[thousand]
-        
-        hundred = (num%1000)//100
-        h = {9:"CM",8:"DCCC",7:"DCC",6:"DC",5:"D",4:"CD",3:"CCC",2:"CC",1:"C",0:""}
-        result += h[hundred]
-        
-        ten = (num%100)//10
-        t2 = {9:"XC",8:"LXXX",7:"LXX",6:"LX",5:"L",4:"XL",3:"XXX",2:"XX",1:"X",0:""}
-        result += t2[ten]
-        
-        one = (num%10)
-        o = {9:"IX",8:"VIII",7:"VII",6:"VI",5:"V",4:"IV",3:"III",2:"II",1:"I",0:""}
-        result += o[one]
-        
-        return result
+    def intToRoman(self, num):
+        values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
+        numerals = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
+        res, i = "", 0
+        while num:
+            res += (num//values[i]) * numerals[i]
+            num %= values[i]
+            i += 1
+        return res
