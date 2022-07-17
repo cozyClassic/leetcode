194d2e72ddd0d28de13d790ec971b72f6d6804b3
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2022-07-17 18:14:26 +0900

    Time: 351 ms (72.63%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,9 +1,5 @@
 # Write your MySQL query statement below
-SELECT DISTINCT P1.email
-FROM Person AS P1
-    LEFT JOIN 
-        (SELECT id, email
-        FROM Person
-        GROUP BY email) AS P2
-    USING (id)
-WHERE P2.id IS NULL
+select Email
+from Person
+group by Email
+having count(*) > 1
