7e3468bbade068853c9855f0f5b7933498193036
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2022-07-18 18:14:43 +0900

    Time: 1426 ms (23.25%), Space: 0B (100.00%) - LeetHub

---
+++ 185-department-top-three-salaries/185-department-top-three-salaries.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT 
+    D.name AS Department,
+    E1.name AS Employee,
+    E1.salary AS Salary
+FROM Department D, Employee E1, Employee E2
+WHERE 
+    D.id = E1.departmentId 
+    AND D.id = E2.departmentId
+    AND E1.salary <= E2.salary
+GROUP BY D.id, E1.name
+HAVING COUNT(DISTINCT E2.salary) <= 3
+ORDER BY E1.salary DESC
