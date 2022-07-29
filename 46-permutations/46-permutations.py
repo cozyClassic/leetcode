3308bd3871f88680a35a28da511bb4c510b03e2c
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2022-07-30 04:32:40 +0900

    Time: 68 ms (37.08%), Space: 13.9 MB (83.89%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,16 +1,4 @@
+from itertools import permutations
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        L = len(nums)
-
-        def dfs(num_list,append_list) :
-            if len(num_list) == L:
-                result.append(num_list)
-                return
-
-            for i in range(len(append_list)):
-                dfs(num_list+[append_list[i]], append_list[:i]+append_list[i+1:])
-
-        dfs([],nums)
-
-        return result
+        return (list(permutations(nums)))
