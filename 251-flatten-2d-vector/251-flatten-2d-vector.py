ecd94100fbb53e29bb49ca9d7d45d2e07fdf1deb
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2022-07-30 13:37:28 +0900

    Time: 166 ms (11.18%), Space: 19.3 MB (12.69%) - LeetHub

--- 251-flatten-2d-vector/251-flatten-2d-vector.py
+++ 251-flatten-2d-vector/251-flatten-2d-vector.py
@@ -1,17 +1,26 @@
 class Vector2D:
+
     def __init__(self, vec: List[List[int]]):
-        self.V = []
-        for ve in vec:
-            self.V.extend(ve)
-        self.P = -1
-        return
+        self.v = vec
+        self.inner = 0
+        self.outer = 0
+        
+    def advance_to_next(self):
+        while self.outer < len(self.v) and self.inner == len(self.v[self.outer]):
+            self.outer += 1
+            self.inner = 0
 
     def next(self) -> int:
-        self.P += 1
-        return self.V[self.P]
+        self.advance_to_next()
+        result = self.v[self.outer][self.inner]
+        self.inner += 1
+        return result
+        
 
     def hasNext(self) -> bool:
-        return self.P < len(self.V) - 1
+        self.advance_to_next()
+        
+        return self.outer < len(self.v)
 
 
 # Your Vector2D object will be instantiated and called as such:
