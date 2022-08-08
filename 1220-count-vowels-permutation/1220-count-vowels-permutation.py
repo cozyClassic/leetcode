8b7df36024bf6afc13e940d1bc908252dd1ac7be
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2022-08-08 10:33:36 +0900

    Time: 224 ms (88.03%), Space: 14.2 MB (68.69%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,32 +1,6 @@
 class Solution:
-    from collections import Counter
-    VOWEL = {
-        "a": ["e","i","u"],
-        "e": ["a","i"],
-        "i": ["e","o"],
-        "o": ["i"],
-        "u": ["i","o"]
-    }
-    def trans_char_nums(self,counter):
-        new_counter = Counter()
-        for V in self.VOWEL :
-            for v in self.VOWEL[V]:
-                new_counter[V] += counter[v]
-        return new_counter        
-        
     def countVowelPermutation(self, n: int) -> int:
-        if n == 1 :
-            return 5
-        elif n == 2:
-            return 10
-        
-        counter = Counter()
-        for v in self.VOWEL:
-            counter[v] = 1
-        
-        for i in range(2,n+1):
-            counter = self.trans_char_nums(counter)
-            
-        answer = sum([counter[v] for v in counter])
-        
-        return answer % (10**9 + 7)
+        a, e, i, o, u = 1, 1, 1, 1, 1
+        for _ in range(n - 1):
+            a, e, i, o, u = e + i + u, a + i, e + o, i, i + o
+        return (a + e + i + o + u) % (10**9 + 7)
