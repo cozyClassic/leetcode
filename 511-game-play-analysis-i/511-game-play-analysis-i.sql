8d3d158d488484db69157c3af2a63d7f779f5d87
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2022-08-11 10:58:47 +0900

    Time: 989 ms (16.54%), Space: 0B (100.00%) - LeetHub

--- 511-game-play-analysis-i/511-game-play-analysis-i.sql
+++ 511-game-play-analysis-i/511-game-play-analysis-i.sql
@@ -1,14 +1,5 @@
 # Write your MySQL query statement below
 SELECT 
-    player_id, 
-    first_login
-FROM (
-    SELECT 
-        player_id,
-        event_date as first_login,
-        dense_rank() over (
-            partition by player_id
-        order by event_date) AS poz
-    FROM Activity
-    ) AS src
-WHERE poz = 1
+    DISTINCT(player_id),
+    MIN(event_date) OVER(PARTITION BY player_id) AS first_login
+FROM Activity
