28c9f7b83e6f90b9941222251a8c744e414e8ef3
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2022-08-16 13:48:09 +0900

    Time: 113 ms (66.76%), Space: 44.5 MB (78.47%) - LeetHub

--- 1-two-sum/1-two-sum.ts
+++ 1-two-sum/1-two-sum.ts
@@ -1,11 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
-    for (let i=0; i<nums.length-1; i++){
-        for (let j=i+1; j<nums.length; j++) {
-            if (nums[i] + nums[j] == target){
-                return [i,j]
-            }
+    let hash:Object = {};
+    let num:number = -1;
+    for (let i=0; i<nums.length; i++){
+        num = nums[i];
+        if (hash[target-num] != undefined) {
+            return [hash[target-num], i];
         }
+        hash[num] = i;        
     }
-    
-    return [-1,-1]
 };
