07c63b168c03ed3af780b473b1737896b1a6c880
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2022-08-16 14:15:48 +0900

    Time: 115 ms (64.78%), Space: 47.3 MB (5.78%) - LeetHub

--- 1-two-sum/1-two-sum.ts
+++ 1-two-sum/1-two-sum.ts
@@ -1,11 +1,21 @@
 function twoSum(nums: number[], target: number): number[] {
-    let hash:Object = {};
-    let num:number = -1;
-    for (let i=0; i<nums.length; i++){
-        num = nums[i];
-        if (hash[target-num] != undefined) {
-            return [hash[target-num], i];
+    // 원본 인덱스 유지하면서 sort하기
+    let index_nums:object[] = [...nums.entries()]
+    index_nums.sort((prev,next) => prev[1] - next[1]) 
+    let left:number = 0
+    let right:number = nums.length-1;
+    
+    while (left < right) {
+        let l = index_nums[left]
+        let r = index_nums[right]
+        let sum:number = l[1] + r[1]
+        if (sum == target){
+            return [l[0], r[0]]
+        } else if (sum < target) {
+            left += 1;
+        } else if (sum > target) {
+            right -= 1;
         }
-        hash[num] = i;        
-    }
+    }   
+    return [1]
 };
