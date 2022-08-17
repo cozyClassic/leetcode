6680b4bd6afeb7850beaef87536e041e4a64b3d8
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2022-08-17 10:29:34 +0900

    Time: 88 ms (67.52%), Space: 43.9 MB (42.34%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.js
+++ 804-unique-morse-code-words/804-unique-morse-code-words.js
@@ -10,14 +10,6 @@ var uniqueMorseRepresentations = function(words) {
     for (let i=0; i<26; i++) {
         hashs[a.next().value] = m.next().value
     }
-    results = new Set()
-    
-    for (const word of words) {
-        let result = []
-        for (const w of word) {
-            result[result.length] = hashs[w]
-        }
-        results.add(result.join(""))
-    }
+    results = new Set(words.map((word) => word.split("").map((char) => hashs[char]).join("")))
     return results.size
 };
