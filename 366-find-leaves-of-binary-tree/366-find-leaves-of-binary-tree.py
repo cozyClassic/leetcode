f12164f3383a1f0d4ea0aaad4534b563a2dd8ec9
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2022-08-22 10:51:54 +0900

    Time: 50 ms (43.60%), Space: 13.9 MB (27.07%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.py
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.py
@@ -1,9 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     leafs = []
     def is_leaf(self, node:TreeNode):
