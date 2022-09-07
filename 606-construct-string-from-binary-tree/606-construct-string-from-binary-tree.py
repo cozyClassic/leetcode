ffa3d307f60d701168a612a26ad7bc59e1b6c4b1
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2022-09-07 14:31:10 +0900

    Time: 91 ms (37.24%), Space: 16.5 MB (30.02%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.py
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.py
@@ -5,23 +5,15 @@ class Solution:
             nonlocal result
             result += "("
             result += str(node.val)
-            
             if node.left :
                 dfs(node.left)
                 result+= ")"
-                
                 if node.right :
                     dfs(node.right)
                     result+= ")"
-            
             elif node.right :
                 result += "()"
                 dfs(node.right)
                 result += ")"
-                
-            else:
-                return
-    
         dfs(root)
-        
         return result[1:]
