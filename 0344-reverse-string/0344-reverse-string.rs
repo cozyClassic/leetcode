92d1680aa397ae27bc165df37baf98a771aef14c
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2023-04-24 19:21:53 +0900

    Time: 18 ms (29.77%), Space: 5.5 MB (31.68%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.rs
+++ 0344-reverse-string/0344-reverse-string.rs
@@ -2,9 +2,7 @@ impl Solution {
     pub fn reverse_string(s: &mut Vec<char>) {
         let L = s.len();
         for index in 0..L/2 {
-            let temp:char = s[index];
-            s[index] = s[L-1-index];
-            s[L-1-index] = temp;
+            s.swap(index, L-1-index)
         }
     }
 }
