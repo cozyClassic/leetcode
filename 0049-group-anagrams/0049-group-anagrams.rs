78562e888d5fbab129b61c8157f8ca7edd4433ad
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2023-05-01 17:42:11 +0900

    Time: 6 ms (89.94%), Space: 5.1 MB (32.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.rs
+++ 0049-group-anagrams/0049-group-anagrams.rs
@@ -10,7 +10,7 @@ impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
         let mut map:HashMap<Vec<char>, Vec<String>> = HashMap::new();
         for s in strs {
-            let values = map.entry(get_key(&s)).or_default().push(s);
+            let values = map.entry(get_key(&s)).or_insert(vec![]).push(s);
         }
         map.into_values().collect()
     }
