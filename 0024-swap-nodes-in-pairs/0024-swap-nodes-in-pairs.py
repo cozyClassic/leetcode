e32f75ef0e804232bdd58510a9028c8265bc558d
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2024-08-07 09:59:48 -0400

    Time: 40 ms (22.61%), Space: 16.5 MB (62.08%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -0,0 +1,17 @@
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        result = Z = ListNode(None)
+        result.next = A = head
+
+        while A and A.next:
+            # save pointer for C
+            B = A.next
+            C = B.next
+            # (Z) -> A -> B => (Z) -> B -> A
+            Z.next, B.next, A.next = B, A, C
+
+            # move pointer (Z) -> B -> A => (A>Z) -> (C->A)
+            Z = A
+            A = C
+
+        return result.next
