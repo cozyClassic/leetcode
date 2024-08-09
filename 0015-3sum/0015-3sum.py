9f2392ed498dd513d088cc0013c5a77162ad0af5
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2024-08-09 10:27:46 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,14 +1,13 @@
 class Solution:
     # nums2 is already sorted
     def twoSum(self, nums2, goal):
-        result = []
+        result = set()
         seen = set()
         for n in nums2:
             remain = goal - n
             if remain in seen:
-                result.append([n, remain])
-            else:
-                seen.add(n)
+                result.add(tuple([-goal, n, remain]))
+            seen.add(n)
         return result
 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
@@ -18,6 +17,5 @@ class Solution:
         for i in range(len(nums)-2):
             n1 = nums[i]
             combs = self.twoSum(nums[i+1:], -n1)
-            for c in combs:
-                result.add(tuple([n1] + c))
-        return result
+            result = result.union(combs)
+        return list(result)
