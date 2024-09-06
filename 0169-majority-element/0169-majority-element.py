5b9c54fc64ec30bca581ddafc135a8d86814a95d
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2024-09-06 14:13:18 -0400

    Time: 209 ms (5.07%), Space: 18.1 MB (22.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,9 +1,24 @@
-from collections import Counter
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        c = Counter(nums)
-        n = len(nums) // 2
-        for key, value in c.items():
-            if value > n:
-                return key
-        return 0
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def divide_conquer(nums,l,r) :
+            if l == r:
+                 return (nums[l], 1)
+            mid = (l + r) // 2
+            f = divide_conquer(nums,l,mid) 
+            s = divide_conquer(nums,mid+1,r)
+            ## not the same majority substract the extra
+            if f[0] != s[0] :
+                if f[1] > s[1] :
+                    return (f[0],f[1]-s[1])
+                else :
+                    return (s[0],s[1]-f[1])
+            else :
+                return (f[0],s[1]+f[1])
+        # print(divide_conquer(nums))
+        n = len(nums)
+        return divide_conquer(nums,0,n-1)[0]
+         
