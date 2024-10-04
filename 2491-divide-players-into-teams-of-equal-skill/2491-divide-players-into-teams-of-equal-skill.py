0393e6f6aea65c769e61b7b0196e631ebeddfc20
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2024-10-04 09:55:46 +0900

    Time: 375 ms (96.28%), Space: 29.6 MB (31.15%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -1,14 +1,15 @@
+from collections import Counter
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
-        if len(skill) == 2: return skill[0] * skill[1]
+        n = len(skill)
 
-        skill.sort()
-        result = 0
-        standard = skill[0] + skill[-1]
-        L = len(skill) - 1
-
-        for i in range((L+1)//2):
-            if skill[i] + skill[L-i] != standard: return -1
-            result += skill[i] * skill[L-i]
-
-        return result
+        target = 2 * sum(skill) / n
+        if target % 1 != 0: return -1
+        counts = Counter(skill)
+        ans = 0
+        for s, c in counts.items():
+            if target - s not in counts: return -1
+            if c != counts[target - s]: return -1
+            total_chem = int(s * (target - s) * c)
+            ans += total_chem
+        return ans >> 1
