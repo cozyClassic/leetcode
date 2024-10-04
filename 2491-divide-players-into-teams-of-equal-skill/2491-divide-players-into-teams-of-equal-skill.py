a346af193229bb318dcb21e8bd322c5db3c48365
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2024-10-04 09:52:35 +0900

    Time: 405 ms (46.9%), Space: 29.4 MB (68.5%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -1,17 +1,13 @@
-from collections import deque
-
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
         if len(skill) == 2: return skill[0] * skill[1]
 
-        skill = deque(sorted(skill))
+        skill.sort()
         result = 0
         standard = skill[0] + skill[-1]
 
-        while skill:
-            if skill[0] + skill[-1] != standard: return -1
-            result += skill[0] * skill[-1]
-            skill.popleft()
-            skill.pop()
+        for i in range(len(skill)//2):
+            if skill[i] + skill[len(skill)-i-1] != standard: return -1
+            result += skill[i] * skill[len(skill)-i-1]
 
         return result
