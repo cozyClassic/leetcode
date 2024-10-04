fcc094a1f6f1cab4dc9449d6c19816fbfd5c1a09
Minchan.Kim <46748877+cozyClassic@users.noreply.github.com>
2024-10-04 09:54:39 +0900

    Time: 401 ms (56.99%), Space: 29.3 MB (94.16%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -5,9 +5,10 @@ class Solution:
         skill.sort()
         result = 0
         standard = skill[0] + skill[-1]
+        L = len(skill) - 1
 
-        for i in range(len(skill)//2):
-            if skill[i] + skill[len(skill)-i-1] != standard: return -1
-            result += skill[i] * skill[len(skill)-i-1]
+        for i in range((L+1)//2):
+            if skill[i] + skill[L-i] != standard: return -1
+            result += skill[i] * skill[L-i]
 
         return result
